f65f06d8ebc67e026d7b94ab1dae553cfeaa382c
Dmitriy <zelentsov.dmitry@gmail.com>
2022-09-01 21:14:46 +0300

    add Ex2

---
+++ Ex2/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу,которая получает число и выводит сумма всех цифр этого числа
+int DigitSum(int number)
+{
+    if (number == 0)
+    return 0;
+
+   return (number % 10) + DigitSum(number/10);
+}
+
+Console.WriteLine(DigitSum(632));
