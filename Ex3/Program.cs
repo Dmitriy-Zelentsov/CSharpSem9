efba85450d07a01b3e7d23c4e2be70185405ff9c
Dmitriy <zelentsov.dmitry@gmail.com>
2022-09-01 21:30:21 +0300

    add Ex3

---
+++ Ex3/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу,которая получает два числа А и В
+// и возводит число А в целую степень В с помощью рекурсии
+
+
+int DigitSum(int number, int step)
+{
+    if (step == 0)
+    return 1;
+
+   return (number*DigitSum(number, step - 1));
+}
+
+Console.WriteLine(DigitSum(2,5));
